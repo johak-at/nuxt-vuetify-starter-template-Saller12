490b57ba8968fab1a4c6dd0d10046e673a7b29a1
Saller12 <saller.josef@johak.at>
2022-12-20 11:18:43 +0100

    v-card
    ja v-card

---
+++ pages/test/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-main>
+        <v-container>
+            <v-card></v-card>
+            <v-card-title>Card-Title</v-card-title>
+            <v-card-subtitle>Card-Subtitle</v-card-subtitle>
+            <v-card-text>Card-Text</v-card-text>
+            <v-card-actions>Card-Actions</v-card-actions>
+            <v-btn></v-btn>
+            <v-btn color="error">Primary</v-btn>
+        </v-container>
+    </v-main>
+</template>
